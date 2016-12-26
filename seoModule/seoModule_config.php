22c9bf6b876f83b532504143fb65613dab9cdfa3
Сергей <scorn17@gmail.com>
2016-12-26 16:01:37 +0300

    Update seoModule_config.php

--- seoModule/seoModule_config.php
+++ seoModule/seoModule_config.php
@@ -1,34 +1,30 @@
 <?php
 /*
-- много сайтовость
-	+ редиректы
-	+ редиректы на основной домен
-- кэш шаблонов
-	+ логи
-	+ логировать ошибки
-	+ выдача по запросу хэш суммы оптимизации
-	+ история хэш сумм
-	+ поиск участка кода для вставки текста ... по тегу // заменить или добавить до или после или класс элемента
-	+ тэг куда вставить ссылку на страницу статьи
-- скрытая оптимизация
-- города - мега модуль
-	-- robots.txt
-	+ WordPress - при залогине в админке - не рабит сайт
-	-- Expires в Netcat
-	-- на S страницах заменять H1
+- тестировать редиректы на POST запросах
+- проход по всем страницам сайта собственными силами сайта
+- sitemap
+- robots.txt
+- ссылка на статьи только на опт.страницах - исправить
 */
 
 
 
 /**
- * Если в ёбаном Битриксе слетает авторизация, удалите его нахуй
- * или Настройки -> Пользователи -> Группы -> Безопасность -> установите маски: 0.0.0.0
+ * include_once('_buran/seoModule.php'); - в начало скрипта-обработчика путей (смотреть .htaccess)
+ *
+ * Если в Битриксе слетает авторизация,
+ * Настройки -> Пользователи -> Группы -> Безопасность -> установите маски: 0.0.0.0
+ *
+ * Собственной функции определения кодировки пока нет. Когда понадобится - добавим.
+ *
+ * Получение контента донора пока только через CURL. Другие методы добавим по мере надобности.
  */
 
 // Redirects --------------------------------------------------------
 $redirects= array(
-	1 => array(
-		'/index.php'         => '/',
+	'global' => array(
+		'/index.html' => '/',
+		'/index.php'  => '/',
 	),
 );
 // Redirects --------------------------------------------------------
@@ -41,44 +37,52 @@ $websites[1]= array(
 	'/page_uri' - URI страницы списка статей
 )
 */
-$websites[1]= array('http://.ru', '/', '/stati/', '/stati/'); 
+$websites= array(
+	1 => array('http://domain.ru', '/', '/', '/'),
+);
 
 $seopages= array(
-	1 => array(
-		'/dfgdfgdfg/'                                               => 'A:dfgdfgdfg',
+	'global' => array(
+		'/dfgdfgdfg/' => 'A:dfgdfgdfg',
 	),
 );
 
-$config= array(
-	'module_enabled'     => true, // Активность модуля ... можно указать IP-адрес 80.80.109.182
-	's_page_suffix'      => '/', // суффикс S статей с точкой в начале
-	'get_content_method' => 'curl', // curl // file_get_contents // socket
-	'tx_path'            => '/tx', // путь к папке со статьями      '/tx'
-	'img_path'           => '/tx/img', // путь к папке с картинками '/tx/img'
-	'use_share'          => true, // блок поделиться
-	'img_width'          => 300,
-	'img_height'         => 200,
-	'checkcharsetmethod' => 'mb_detect_encoding', // mb_detect_encoding // own_function - метод определения кодировки
-	'toencoding'         => 'utf-8', // целевая кодировка текстов
-	'base'               => 'replace_or_add', // replace_or_add // replace_if_exists // delete // false
-	'canonical'          => 'replace_or_add', // replace_or_add // replace_if_exists // delete // false
-	'meta'               => 'replace_or_add', // replace_or_add // replace_if_exists // delete // false
-	'requets_methods'    => '/GET/HEAD/',
-	'https_test'         => false, // true - для тестирования оптимизации на домене с HTTPS, но без сертификата
+$configs= array(
+	'global' => array(
+		'debug'              => false, // влючать только на время тестирования
+		'module_enabled'     => true, // активность модуля ... можно указать IP-адрес '80.80.109.182'
+		's_page_suffix'      => '.html', // суффикс S статей
+		'get_content_method' => 'curl', // curl // file_get_contents // socket
+		'tx_path'            => '/tx', // путь к папке со статьями      '/tx'
+		'img_path'           => '/tx/img', // путь к папке с картинками '/tx/img'
+		'use_share'          => true, // блок поделиться
+		'img_width'          => 300,
+		'img_height'         => 200,
+		'checkcharsetmethod' => 'mb_detect_encoding', // mb_detect_encoding // own_function - метод определения кодировки
+		'toencoding'         => 'utf-8', // целевая кодировка текстов
+		'base'               => 'replace_or_add', // replace_or_add // replace_if_exists // delete // false
+		'canonical'          => 'replace_or_add', // replace_or_add // replace_if_exists // delete // false
+		'meta'               => 'replace_or_add', // replace_or_add // replace_if_exists // delete // false
+		'requets_methods'    => '/GET/HEAD/',
+		'https_test'         => false, // true - для тестирования оптимизации на домене с HTTPS, но без сертификата
+		'display_errors'     => 'on', // 'on' | 'off' - ini_set('display_errors');
+		'error_reporting'    => E_ALL & ~E_NOTICE, // error_reporting();
+	),
 );
 
-$config['share_code']= '<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script><script src="//yastatic.net/share2/share.js"></script><div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,skype,telegram" data-counter=""></div>';
+$configs['global']['share_code']= '<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script><script src="//yastatic.net/share2/share.js"></script><div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,skype,telegram" data-counter=""></div>';
 
-$seo_text_styles= '<style>
+$configs['global']['styles']= '<style>
 	.sssmodulebox {padding:0 0 20px; font-size:1em; line-height:1em;}
-	.sssmodulebox .sssmb_h1 h1 {margin:1.3em 0 1em;}
+	.sssmodulebox .sssmb_h1 h1 {margin:1.2em 0 1em; line-height:1.2em;}
 	.sssmodulebox .sssmb_stext {padding:0 0 20px}
 	.sssmodulebox .sssmb_stext h2,
 	.sssmodulebox .sssmb_stext h3,
-	.sssmodulebox .sssmb_stext h4 {margin:1.2em 0 1em;}
+	.sssmodulebox .sssmb_stext h4 {margin:1.2em 0 1em; line-height:1.3em;}
 	.sssmodulebox .sssmb_stext p {line-height:1.5em; padding:5px 0;}
 	.sssmodulebox .sssmb_stext ul {list-style:disc; margin:1em 0 1em 2em;}
-	.sssmodulebox .sssmb_stext ul li {display:list-item; margin:0; padding:5px 0 5px 1em;}
+	.sssmodulebox .sssmb_stext ul li {display:list-item; margin:0; padding:5px 0 5px 1em; line-height:1.2em;}
+	.sssmodulebox .yasharebox ul li {background:none; padding-left:0;}
 </style>';
 
 /*
@@ -91,14 +95,14 @@ $seo_text_styles= '<style>
  * 		# - добавить ПОСЛЕ
  */
 
-$content_start[]= '#<!--content_start-->';
+$content_start['global'][]= '#<!--content_start-->';
 
 // --------------------
 
-$content_finish[]= '%<!--content_end-->';
+$content_finish['global'][]= '%<!--content_end-->';
 
 // --------------------
 
-// $articles_link[]= '#';
+// $articles_link['global'][]= '#';
 
 // --------------------
