2d1c3ad5ba69f82f0503eb39bae931220f25b004
Сергей <scorn17@gmail.com>
2016-12-28 11:32:44 +0300

    Update seoModule.php

--- seoModule/seoModule.php
+++ seoModule/seoModule.php
@@ -239,7 +239,7 @@ if(
 
 				if($seotype=='A')
 				{
-					$template= preg_replace("/<h1(.*)>(.*)<\/h1>/iU", '<h2 ${1}>${2}</h2>', $template);
+					$template= preg_replace("/<h1(.*)>(.*)<\/h1>/isU", '<h2 ${1}>${2}</h2>', $template);
 					if(preg_last_error()) tolog('[error_09]','errors');
 					if($cf_cc===1)
 					{
@@ -289,11 +289,11 @@ if(
 						$meta_description= '';
 						$meta_keywords= '';
 					}
-					$template= preg_replace("/<meta (.*)name=('|\")description('|\")(.*)>/iU", $meta_description, $template,1);
+					$template= preg_replace("/<meta (.*)name=('|\")description('|\")(.*)>/isU", $meta_description, $template,1);
 					if(preg_last_error()) tolog('[error_13]','errors');
-					$template= preg_replace("/<meta (.*)name=('|\")keywords('|\")(.*)>/iU", $meta_keywords, $template,1);
+					$template= preg_replace("/<meta (.*)name=('|\")keywords('|\")(.*)>/isU", $meta_keywords, $template,1);
 					if(preg_last_error()) tolog('[error_14]','errors');
-					$template= preg_replace("/<title>(.*)<\/title>/iU", $meta_title, $template,1);
+					$template= preg_replace("/<title>(.*)<\/title>/isU", $meta_title, $template,1);
 					if(preg_last_error()) tolog('[error_15]','errors');
 				}
 
