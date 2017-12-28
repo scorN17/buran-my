2a43407d15a5d4f0e28abf84a90ed5147b6dcc3e
Сергей <scorn17@gmail.com>
2017-12-28 09:07:09 +0300

    Update __012.php

--- _buran/__012.php
+++ _buran/__012.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Buran_012
- * v1.0
- * 11.12.2017
+ * v1.11
+ * 28.12.2017
  * Delta
  * sergey.it@delta-ltd.ru
  *
@@ -10,18 +10,19 @@
  * php_value auto_prepend_file /.../_buran/__012.php
  */
 
-if(is_array($_POST) && count($_POST))
-{
+if (is_array($_POST) && count($_POST)) {
 	$droot= dirname(__FILE__);
-	if( ! @file_exists($droot.'/b012/'))
+	if ( ! @file_exists($droot.'/b012/')) {
 		@mkdir($droot.'/b012/', 0777, true);
+	}
 	$fp= @fopen($droot.'/b012/'.$_SERVER['REMOTE_ADDR'], 'ab');
-	if($fp)
-	{
+	if ($fp) {
 		@fwrite($fp, "\n".'= '.date('d.m.Y, H:i:s').' ='."\n");
 		@fwrite($fp, "\t".'+ '.$_SERVER['REQUEST_URI']."\n");
-		foreach($_POST AS $k => $v)
-			@fwrite($fp, "\t| ".$k.' => '.$v."\n");
+		foreach ($_POST AS $k => $v) {
+			@fwrite($fp, "\t| ". $k .' => ');
+			@fwrite($fp, (is_array($v) ? print_r($v,1) : $v) ."\n");
+		}
 		@fclose($fp);
 	}
 }
